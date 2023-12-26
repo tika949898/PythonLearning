7af1acc61d0cdaef122c387e2a592e2f6143a361
Tikaram MahtoTikaram Mahto <tika949898@gmail.com>
2023-12-26 20:24:42 +0530

    Today

---
+++ range.py
@@ -0,0 +1,9 @@
+        numbers = (1, 10, 2)
+
+for i in range(numbers[0], numbers[1], numbers[2]):
+    if i == 5:
+        print('Very good!')
+    else:
+        print('This is very bad')
+
+print('Happy birthday!')
